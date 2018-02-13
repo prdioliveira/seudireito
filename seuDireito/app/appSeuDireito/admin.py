1b3cc2f1572914a8dcf6b364b66d2e5aea6b41db
prdioliveira <prdioliveira@gmail.com>
2018-02-12 22:46:05 -0200

    registrando os models no admin

--- seuDireito/app/appSeuDireito/admin.py
+++ seuDireito/app/appSeuDireito/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Advogado, Empresa
+from .models import Advogado, Empresa, Status, OrdemServico, Proposta
 
 # Register your models here.
 admin.site.register(Advogado)
 admin.site.register(Empresa)
+admin.site.register(Status)
+admin.site.register(OrdemServico)
+admin.site.register(Proposta)
